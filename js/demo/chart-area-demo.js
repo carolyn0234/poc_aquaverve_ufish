4936c68375e57d705b877402302981016adb3045
carolyn0234 <carolyn.tan2002@gmail.com>
2023-12-06 22:47:40 +0800

    update area chart

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -216,6 +216,30 @@ fetchData().then((data) => {
           }
         }],
       },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        backgroundColor: "rgb(255,255,255)",
+        bodyFontColor: "#858796",
+        titleMarginBottom: 10,
+        titleFontColor: '#6e707e',
+        titleFontSize: 14,
+        borderColor: '#dddfeb',
+        borderWidth: 1,
+        xPadding: 15,
+        yPadding: 15,
+        displayColors: false,
+        intersect: false,
+        mode: 'index',
+        caretPadding: 10,
+        callbacks: {
+          label: function (tooltipItem, chart) {
+            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+            return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+          }
+        }
+      }
     },
   });
 });
